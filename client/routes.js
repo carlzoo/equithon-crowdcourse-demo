6d75b48bd9aed5b3133c359611eb85591a27476b
Jennifer Chan <jenniferrxchan@gmail.com>
2018-05-05 17:50:03 -0400

    update

--- client/routes.js
+++ client/routes.js
@@ -1,10 +1,23 @@
-// Router.route('/signup');
-Router.configure({
-  // layoutTemplate: 'content',
+if(Meteor.isClient) {
+  Router.configure({
+    // the default layout
+    layoutTemplate: 'empty'
+  });
 
-  // template: 'content'
+  Router.route('/', function () {
+    console.log("home page route");
+    this.render('content');
+    this.layout('navbar');
+  });
 
-  // .
-  // .
-  // .
-});
+
+  Router.route('/signup', function () {
+    this.render('signup');
+    this.layout('navbar');
+  });
+
+  Router.route('/explore', function () {
+    this.render('explore');
+    this.layout('topnav1');
+  });
+}
