66914649ea52727e3cc57f172f2115da61ccad90
Carl Zhou <carl.j.zhou@protonmail.ch>
2018-05-05 22:14:58 -0400

    add edit content

--- client/moar-routes.js
+++ client/moar-routes.js
@@ -2,3 +2,8 @@ Router.route('/course/1', function () {
     this.render('coursecontent');
     this.layout('topnav1');
   });
+
+  Router.route('/course/1/edit', function () {
+    this.render('editcoursecontent');
+    this.layout('topnav1');
+  });
