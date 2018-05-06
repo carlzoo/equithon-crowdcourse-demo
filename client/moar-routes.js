a9d238cdc97193a829283b4640fd9d859dbcd24b
Carl Zhou <carl.j.zhou@protonmail.ch>
2018-05-05 23:54:06 -0400

    add missing buttons

--- client/moar-routes.js
+++ client/moar-routes.js
@@ -1,9 +1,14 @@
 Router.route('/course/1', function () {
     this.render('coursecontent');
-    this.layout('topnav1');
+    this.layout('user-topnav');
   });
 
   Router.route('/course/1/edit', function () {
     this.render('editcoursecontent');
-    this.layout('topnav1');
+    this.layout('user-topnav');
+  });
+
+Router.route('/course/1/edit/2', function () {
+    this.render('pendingedit');
+    this.layout('user-topnav');
   });
